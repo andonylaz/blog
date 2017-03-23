4a547f4f4d40b3d4b75319b84b265a32e41a2529
monash <monash>
2017-03-23 18:36:45 +1100

    Migrated database and have added models to admin page

--- mozilla_assessment/blog/admin.py
+++ mozilla_assessment/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Author, Blog, Comment
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Author, AuthorAdmin)
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Blog, BlogAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Comment, CommentAdmin)
